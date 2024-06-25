d6a04e8047a96b97da9ad76ffb246a8736147d18
JoseAlberto <josealberto@gmail.com>
2024-06-24 22:02:23 -0600

    Crear Constancia

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get("/loginJefe", (req, res) => {
 */
 
 //Edicion de documentos
-app.get("/documentos/:numero_control", (req, res) => {
+app.get("/crearConstancia/:numero_control", (req, res) => {
   const numero_control = req.params.numero_control;
 
   conection.query(
